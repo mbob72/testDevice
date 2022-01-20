df3ffc72d8772c753edfdabf420478a7d0874b79
Mikhail Bobryshev <MBobryshev@luxoft.com>
2022-01-20 13:39:04 +0300

    add app version

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>AppVersion: {navigator.appVersion}</div>
         <img src={logo} className="App-logo" alt="logo" />
         <div>{`Ваш браузер ${ifWrong ? 'НЕ' : ''} поддерживается`}</div>
         <div>{ifHigher15 && 'Ваша платформа = xOS, ее версия  15.1.x'}</div>
